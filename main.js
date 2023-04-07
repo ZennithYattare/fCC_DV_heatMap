9387f7f94deaa0c80d278c50c65c2a25f06a7531
ZennithYattare <luis.espera.0758@gmail.com>
2023-04-08 07:26:37 +0800

    adjust width

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ import { heatMap } from "./heatMap.js";
 
 document.querySelector("#app").innerHTML = `
   <div class="bg-[#242424] flex h-screen w-screen place-items-center">
-    <div class="min-w-[960px] w-[960px] h-[650px] border-4 border-t-slate-200 border-l-slate-200 border-r-[#808080] border-b-[#808080] bg-[#c0c0c0] shadow-[5px_5px_5px_black] mx-auto p-4">
-      <div id="title" class="text-4xl mx-auto w-max">Something</div>
+    <div class="min-w-[1500px] w-[1500px] h-[650px] border-4 border-t-slate-200 border-l-slate-200 border-r-[#808080] border-b-[#808080] bg-[#c0c0c0] shadow-[5px_5px_5px_black] mx-auto p-4">
+      <heading>
+        <h1 id="title" class="text-3xl mx-auto w-max">Monthly Global Temperature</h1>
+        <h3 id="description" class="mx-auto w-max">1753-2015: base temperature 8.66℃</h3>
+      </heading>
       <div id="heat-map" class="m-0 p-0"></div>
     </div>
   </div>
